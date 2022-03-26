ea796664327307f1eddb43fa14b50cf61fe56a7a
Klay Clarke <clarkeklay@gmail.com>
2022-03-26 10:28:48 -0400

    add user create api capability - hashes user password using bcrypt

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -34,24 +34,24 @@ exports.user_create = [
   body("password").trim().isLength({ min: 1 }).escape(),
   (req, res, next) => {
     const errors = validationResult(req);
-    let hashedPassword = "";
-    bcrypt.hash(req.body.password, 10, (err, returnedHashedPassword) => {
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
       if (err) return next(err);
-      hashedPassword = returnedHashedPassword;
-    });
-    let user = new User({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-    if (!errors.isEmpty()) return res.json(errors.array());
-    User.findOne({ email: req.body.email }).exec(function (err, found_user) {
-      if (err) return next(err);
-      if (found_user) return res.send("User with same email found in database");
-      user.save(function (err) {
+      let user = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      if (!errors.isEmpty()) return res.json(errors.array());
+      User.findOne({ email: req.body.email }).exec(function (err, found_user) {
         if (err) return next(err);
-        res.send("User successfully created");
+        if (found_user)
+          return res.send("User with same email found in database");
+        user.save(function (err) {
+          if (err) return next(err);
+          res.send("User successfully created");
+        });
       });
     });
   },
