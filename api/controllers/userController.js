fff5222676380e8f6e7653bf2f95d2d6aaf7da6f
Klay Clarke <clarkeklay@gmail.com>
2022-03-26 10:28:35 -0400

    add user create api capability - hashes user password using bcrypt

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -27,7 +27,35 @@ exports.user_detail = function (req, res, next) {
 };
 
 // user create
-exports.user_create = function (req, res) {};
+exports.user_create = [
+  body("first_name").trim().isLength({ min: 1 }).escape(),
+  body("last_name").trim().isLength({ min: 1 }).escape(),
+  body("email").trim().isLength({ min: 1 }).escape(),
+  body("password").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    let hashedPassword = "";
+    bcrypt.hash(req.body.password, 10, (err, returnedHashedPassword) => {
+      if (err) return next(err);
+      hashedPassword = returnedHashedPassword;
+    });
+    let user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    if (!errors.isEmpty()) return res.json(errors.array());
+    User.findOne({ email: req.body.email }).exec(function (err, found_user) {
+      if (err) return next(err);
+      if (found_user) return res.send("User with same email found in database");
+      user.save(function (err) {
+        if (err) return next(err);
+        res.send("User successfully created");
+      });
+    });
+  },
+];
 
 // user update
 exports.user_update = function (req, res) {};
