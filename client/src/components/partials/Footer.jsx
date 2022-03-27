ac180476f9ec7eab231fe602d5e5b9abfd331181
Klay Clarke <clarkeklay@gmail.com>
2022-03-27 12:13:21 -0400

    add mock code for footer partial

---
+++ client/src/components/partials/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>Twitter</li>
+        <li>Twitter</li>
+        <li>Twitter</li>
+        <li>Twitter</li>
+        <li>Twitter</li>
+        <li>Twitter</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
