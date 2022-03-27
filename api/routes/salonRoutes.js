b83f84ee4f4a32fce765d040215aaf000a1633af
Klay Clarke <clarkeklay@gmail.com>
2022-03-26 22:17:31 -0400

    add salon creation and deletion api calls on the salon router

--- api/routes/salonRoutes.js
+++ api/routes/salonRoutes.js
@@ -6,4 +6,8 @@ router.get("/", salon_controller.salon_list);
 
 router.get("/:salonid", salon_controller.salon_detail);
 
+router.post("/", salon_controller.salon_create);
+
+router.delete("/:salonid", salon_controller.salon_delete);
+
 module.exports = router;
