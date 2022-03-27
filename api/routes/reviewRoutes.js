ea2d608c23cb23efa8529c5127e50c1ed4d1e130
Klay Clarke <clarkeklay@gmail.com>
2022-03-26 22:17:00 -0400

    connect review creation and deletion api calls to review router

--- api/routes/reviewRoutes.js
+++ api/routes/reviewRoutes.js
@@ -6,4 +6,8 @@ router.get("/", review_controller.review_list);
 
 router.get("/:reviewid", review_controller.review_detail);
 
+router.post("/", review_controller.review_create);
+
+router.delete('/"reviewid', review_controller.review_delete);
+
 module.exports = router;
