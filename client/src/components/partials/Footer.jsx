581c392dc5b50b1f9354eb7f0818d42552f45ff8
Klay Clarke <clarkeklay@gmail.com>
2022-03-27 20:30:16 -0400

    add a footer component to application (render on each route)

--- client/src/components/partials/Footer.jsx
+++ client/src/components/partials/Footer.jsx
@@ -2,13 +2,31 @@ function Footer() {
   return (
     <footer>
       <ul>
-        <li>Twitter</li>
-        <li>Twitter</li>
-        <li>Twitter</li>
-        <li>Twitter</li>
-        <li>Twitter</li>
-        <li>Twitter</li>
+        <li>
+          <a href="#">Github</a>
+        </li>
+        <li>
+          <a href="#">LinkedIn</a>
+        </li>
+        <li>
+          <a href="#">Instagram</a>
+        </li>
+        <li>
+          <a href="#">Twitter</a>
+        </li>
       </ul>
+      <form action="">
+        <input
+          type="email"
+          name=""
+          id=""
+          placeholder="Enter Your Email"
+          className="border-radius-10px"
+        />
+        <button type="button" className="button button_coral border-radius-6px">
+          Join Newsletter
+        </button>
+      </form>
     </footer>
   );
 }
