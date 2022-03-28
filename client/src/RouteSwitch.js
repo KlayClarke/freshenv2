badb9ef736ef3ea56b60ee03da0f9b3dcd2b26a3
Klay Clarke <clarkeklay@gmail.com>
2022-03-27 20:29:36 -0400

    place routes into containers for better css style control

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
+import Home from "./components/Home";
 import Footer from "./components/partials/Footer";
 import Nav from "./components/partials/Nav";
 import SalonDetail from "./components/salon/SalonDetail";
@@ -8,18 +8,22 @@ import SalonList from "./components/salon/SalonList";
 function RouteSwitch() {
   return (
     <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/salons" element={<SalonList />} />
-        <Route path="/salons/:salonid" element={<SalonDetail />} />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-      </Routes>
-      <Footer />
+      <div id="page-container">
+        <Nav />
+        <main>
+          <div id="main-content-container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/salons" element={<SalonList />} />
+              <Route path="/salons/:salonid" element={<SalonDetail />} />
+              <Route path="/login" />
+              <Route path="/join" />
+              <Route path="/about" />
+            </Routes>
+          </div>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
