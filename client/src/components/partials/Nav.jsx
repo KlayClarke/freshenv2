426943e695c843f114cc29bc9bc78d002affce79
Klay Clarke <clarkeklay@gmail.com>
2022-03-29 18:09:21 -0400

    when user clicks login and join buttons in nav, modals pop up (these modals will house the respective login and join forms)

--- client/src/components/partials/Nav.jsx
+++ client/src/components/partials/Nav.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import LoginModal from "react-modal";
+import JoinModal from "react-modal";
+
+LoginModal.setAppElement("#root");
+JoinModal.setAppElement("#root");
 
 function Nav() {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isJoinOpen, setIsJoinOpen] = useState(false);
+
+  function toggleLoginModal() {
+    if (isJoinOpen) {
+      setIsJoinOpen(!isJoinOpen);
+    }
+    setIsLoginOpen(!isLoginOpen);
+  }
+
+  function toggleJoinModal() {
+    if (isLoginOpen) {
+      setIsLoginOpen(!isLoginOpen);
+    }
+    setIsJoinOpen(!isJoinOpen);
+  }
+
   return (
     <nav>
       <ul>
@@ -21,17 +44,40 @@ function Nav() {
           <span className="divider"></span>
         </li>
         <li>
-          <Link to={"/login"}>Log In</Link>
+          <Link to={"/login"} onClick={toggleLoginModal}>
+            Log In
+          </Link>
         </li>
         <li>
           <Link
             to={"/join"}
+            onClick={toggleJoinModal}
             className="button button-s header_join-button border-radius-6px"
           >
             Join
           </Link>
         </li>
       </ul>
+      <LoginModal
+        isOpen={isLoginOpen}
+        onRequestClose={toggleLoginModal}
+        contentLabel="My dialog"
+        portalClassName="login-modal"
+        overlayClassName="login-modal-overlay"
+      >
+        <div>My login modal dialog</div>
+        <button onClick={toggleLoginModal}>Close login modal</button>
+      </LoginModal>
+      <JoinModal
+        isOpen={isJoinOpen}
+        onRequestClose={toggleJoinModal}
+        contentLabel="My dialog"
+        portalClassName="join-modal"
+        overlayClassName="join-modal-overlay"
+      >
+        <div>My join modal dialog</div>
+        <button onClick={toggleJoinModal}>Close join modal</button>
+      </JoinModal>
     </nav>
   );
 }
