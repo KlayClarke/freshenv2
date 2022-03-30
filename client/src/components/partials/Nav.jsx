5bfb8389437438d51cfd1575d2024625e67c45c0
Klay Clarke <clarkeklay@gmail.com>
2022-03-30 11:07:29 -0400

    make logo lowercase, remove login and join modal code

--- client/src/components/partials/Nav.jsx
+++ client/src/components/partials/Nav.jsx
@@ -1,35 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import LoginModal from "react-modal";
-import JoinModal from "react-modal";
-
-LoginModal.setAppElement("#root");
-JoinModal.setAppElement("#root");
 
 function Nav() {
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-  const [isJoinOpen, setIsJoinOpen] = useState(false);
-
-  function toggleLoginModal() {
-    if (isJoinOpen) {
-      setIsJoinOpen(!isJoinOpen);
-    }
-    setIsLoginOpen(!isLoginOpen);
-  }
-
-  function toggleJoinModal() {
-    if (isLoginOpen) {
-      setIsLoginOpen(!isLoginOpen);
-    }
-    setIsJoinOpen(!isJoinOpen);
-  }
-
   return (
     <nav>
       <ul>
         <li>
           <Link to={"/"} id="header_logo">
-            FRESHEN
+            freshen
           </Link>
         </li>
       </ul>
@@ -44,14 +22,11 @@ function Nav() {
           <span className="divider"></span>
         </li>
         <li>
-          <Link to={"/login"} onClick={toggleLoginModal}>
-            Log In
-          </Link>
+          <Link to={"/login"}>Log In</Link>
         </li>
         <li>
           <Link
             to={"/join"}
-            onClick={toggleJoinModal}
             className="button button-s header_join-button border-radius-6px"
           >
             Join
