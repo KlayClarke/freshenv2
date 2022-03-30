08633cb5e63fad0759b423048226ceb24522c62c
Klay Clarke <clarkeklay@gmail.com>
2022-03-30 11:10:13 -0400

    add join-content-wrapper div around join-content-container div (allows us to center join form easier) on join route

--- client/src/components/user/JoinForm.jsx
+++ client/src/components/user/JoinForm.jsx
@@ -1,26 +1,47 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+
 function JoinForm() {
   return (
-    <div className="join-content-container">
-      <form action="">
-        <div className="form-section">
-          <label htmlFor="display-name">Display Name</label>
-          <br />
-          <input type="text" id="display-name" />
-          <br />
-        </div>
-        <div className="form-section">
-          <label htmlFor="email">Email</label>
-          <br />
-          <input type="text" id="email" />
-          <br />
-        </div>
-        <div className="form-section">
-          <label htmlFor="password">Password</label>
-          <br />
-          <input type="text" id="password" />
-          <br />
-        </div>
-      </form>
+    <div className="join-content-wrapper">
+      <div className="join-content-container">
+        <button className="auth-join button border-radius-4px">
+          <FontAwesomeIcon
+            icon={faGoogle}
+            className="google-icon"
+          ></FontAwesomeIcon>{" "}
+          Join with Google
+        </button>
+        <form action="">
+          <div className="form-section">
+            <label htmlFor="first-name">First Name</label>
+            <br />
+            <input type="text" id="first-name" />
+            <br />
+          </div>
+          <div className="form-section">
+            <label htmlFor="last-name">Last Name</label>
+            <br />
+            <input type="text" id="last-name" />
+            <br />
+          </div>
+          <div className="form-section">
+            <label htmlFor="email">Email</label>
+            <br />
+            <input type="email" id="email" />
+            <br />
+          </div>
+          <div className="form-section">
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="password" id="password" />
+            <br />
+          </div>
+          <button className="auth-form-submit button border-radius-4px">
+            Join
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
