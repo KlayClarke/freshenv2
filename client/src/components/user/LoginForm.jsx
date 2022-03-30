ba617804b045126ad0a5a2faa408499e134a519d
Klay Clarke <clarkeklay@gmail.com>
2022-03-30 11:10:47 -0400

    add login-content-wrapper div around login-content-container div (allows us to center login form easier) on login route

--- client/src/components/user/LoginForm.jsx
+++ client/src/components/user/LoginForm.jsx
@@ -1,20 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+
 function LoginForm() {
   return (
-    <div className="login-content-container">
-      <form action="">
-        <div className="form-section">
-          <label htmlFor="email">Email</label>
-          <br />
-          <input type="text" id="email" />
-          <br />
-        </div>
-        <div className="form-section">
-          <label htmlFor="password">Password</label>
-          <br />
-          <input type="text" id="password" />
-          <br />
-        </div>
-      </form>
+    <div className="login-content-wrapper">
+      <div className="login-content-container">
+        <button className="auth-login button border-radius-4px">
+          <FontAwesomeIcon
+            icon={faGoogle}
+            className="google-icon"
+          ></FontAwesomeIcon>{" "}
+          Log in with Google
+        </button>
+        <form action="">
+          <div className="form-section">
+            <label htmlFor="email">Email</label>
+            <br />
+            <input type="email" id="email" />
+            <br />
+          </div>
+          <div className="form-section">
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="password" id="password" />
+            <br />
+          </div>
+          <button className="auth-form-submit button border-radius-4px">
+            Log In
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
