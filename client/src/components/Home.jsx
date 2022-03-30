12640c66e5a5e6cac8c24c0c0fcb7def533c7ca6
Klay Clarke <clarkeklay@gmail.com>
2022-03-30 11:06:05 -0400

    add a home-content-wrapper div around the home-content-container div (allows us to center home content easier) on homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,51 +1,39 @@
-import { useState, useEffect, useRef } from "react";
-
 function Home() {
   return (
-    <div className="home-content-container">
-      <h1>
-        FOR THOSE STRUGGLING TO FIND A TRUSTWORTHY BARBERSHOP OR SALON NEARBY
-      </h1>
-      <h2>
-        Freshen serves as both an assistant and an intermediary - connecting
-        trustworthy hairstylists to potential clientele across the nation.
-      </h2>
-      <form action="">
-        <a
-          href="/join"
-          className="button button-l home_get-started-button border-radius-6px"
-        >
-          Get Started
-        </a>
-        <a
-          href="/login"
-          className="button button-l home_login-button border-radius-6px"
-        >
-          Log In
-        </a>
-      </form>
-      <div className="coming-soon-container">
-        <h2>We are currently working to implement the following features:</h2>
-        <ul>
-          <li>
-            <h3>Overall webpage styling</h3>
-          </li>
-          <li>
-            <h3>Login and Join Form styling</h3>
-          </li>
-          <li>
-            <h3>Ability to Join using gmail</h3>
-          </li>
-          <li>
-            <h3>Salon index page styling</h3>
-          </li>
-          <li>
-            <h3>Salon detail page styling</h3>
-          </li>
-          <li>
-            <h3>Scheduling and appointment handling</h3>
-          </li>
-        </ul>
+    <div className="home-content-wrapper">
+      <div className="home-content-container">
+        <h1>
+          FOR THOSE STRUGGLING TO FIND A REPUTABLE BARBERSHOP OR SALON NEARBY
+        </h1>
+        <h2>
+          As both an assistant and an intermediary, we promise to connect
+          trustworthy hairstylists to potential clientele around the world.
+        </h2>
+        <form action="">
+          <a
+            href="/join"
+            className="button button-l home_get-started-button border-radius-6px"
+          >
+            Create A New Account
+          </a>
+          <a
+            href="/login"
+            className="button button-l home_login-button border-radius-6px"
+          >
+            Log In Using An Existing Account
+          </a>
+        </form>
+        <div className="coming-soon-container">
+          <h2>We are currently working to implement the following features:</h2>
+          <ul>
+            <li>
+              <h3>Ability to Join using gmail</h3>
+            </li>
+            <li>
+              <h3>Scheduling and appointment handling</h3>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
