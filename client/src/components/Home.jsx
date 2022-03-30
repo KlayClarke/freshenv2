25e64b397c4d2cadc5f8ad51919cb29530f836ec
Klay Clarke <clarkeklay@gmail.com>
2022-03-30 09:35:36 -0400

    homepage styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,52 @@
 import { useState, useEffect, useRef } from "react";
 
 function Home() {
-  const [apiResponse, setApiResponse] = useState();
-  const [testState, setTestState] = useState("testing");
-
-  async function callApi() {
-    let response = await fetch(process.env.REACT_APP_API_ENDPOINT);
-    let data = await response.text();
-    setApiResponse(data);
-  }
-
-  useEffect(() => {
-    callApi();
-  });
-
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-content-container">
+      <h1>
+        FOR THOSE STRUGGLING TO FIND A TRUSTWORTHY BARBERSHOP OR SALON NEARBY
+      </h1>
+      <h2>
+        Freshen serves as both an assistant and an intermediary - connecting
+        trustworthy hairstylists to potential clientele across the nation.
+      </h2>
+      <form action="">
+        <a
+          href="/join"
+          className="button button-l home_get-started-button border-radius-6px"
+        >
+          Get Started
+        </a>
+        <a
+          href="/login"
+          className="button button-l home_login-button border-radius-6px"
+        >
+          Log In
+        </a>
+      </form>
+      <div className="coming-soon-container">
+        <h2>We are currently working to implement the following features:</h2>
+        <ul>
+          <li>
+            <h3>Overall webpage styling</h3>
+          </li>
+          <li>
+            <h3>Login and Join Form styling</h3>
+          </li>
+          <li>
+            <h3>Ability to Join using gmail</h3>
+          </li>
+          <li>
+            <h3>Salon index page styling</h3>
+          </li>
+          <li>
+            <h3>Salon detail page styling</h3>
+          </li>
+          <li>
+            <h3>Scheduling and appointment handling</h3>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
