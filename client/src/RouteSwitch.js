fa0d26deb0c5a1c7540152a9f77867922a16b22b
Klay Clarke <clarkeklay@gmail.com>
2022-03-30 09:35:20 -0400

    main-content-container as a class instead of id

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -4,26 +4,26 @@ import Footer from "./components/partials/Footer";
 import Nav from "./components/partials/Nav";
 import SalonDetail from "./components/salon/SalonDetail";
 import SalonList from "./components/salon/SalonList";
+import JoinForm from "./components/user/JoinForm";
+import LoginForm from "./components/user/LoginForm";
 
 function RouteSwitch() {
   return (
     <BrowserRouter>
-      <div id="page-container">
-        <Nav />
-        <main>
-          <div id="main-content-container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/salons" element={<SalonList />} />
-              <Route path="/salons/:salonid" element={<SalonDetail />} />
-              <Route path="/login" />
-              <Route path="/join" />
-              <Route path="/about" />
-            </Routes>
-          </div>
-        </main>
-        <Footer />
-      </div>
+      <Nav />
+      <main>
+        <div className="main-content-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/salons" element={<SalonList />} />
+            <Route path="/salons/:salonid" element={<SalonDetail />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/join" element={<JoinForm />} />
+            <Route path="/about" />
+          </Routes>
+        </div>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
 }
