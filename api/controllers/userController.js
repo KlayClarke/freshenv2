05101d41c8986b24b7dd45e03c32bc96dfce7198
Klay Clarke <clarkeklay@gmail.com>
2022-04-01 21:04:57 -0400

    handle user login on user controller - escape user entries and check whether user entered email is tied to a user in db. If not, return error json. If so, check whether user entered password is same as password in db for particular user (using bcrypts compare function to compare the hashed password to the plain text provided by said user)

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const async = require("async");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 const { body, validationResult, sanitizeBody } = require("express-validator");
 
 // return all users
@@ -32,28 +34,25 @@ exports.user_create = [
   body("last_name").trim().isLength({ min: 1 }).escape(),
   body("email").trim().isLength({ min: 1 }).escape(),
   body("password").trim().isLength({ min: 1 }).escape(),
-  (req, res, next) => {
+  async (req, res, next) => {
     const errors = validationResult(req);
-
-    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+    let hashedPassword = await bcrypt.hash(req.body.password, 10);
+    let user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    if (!errors.isEmpty()) return res.json(errors.array());
+    User.findOne({ email: req.body.email }).exec(function (err, found_user) {
       if (err) return next(err);
-      let user = new User({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: hashedPassword,
-      });
-      if (!errors.isEmpty()) return res.json(errors.array());
-      User.findOne({ email: req.body.email }).exec(function (err, found_user) {
-        if (err) return next(err);
-        if (found_user)
-          return res.json({
-            error: "User with same email found in database",
-          });
-        user.save(function (err) {
-          if (err) return next(err);
-          res.json({ success: "User successfully created" });
+      if (found_user)
+        return res.json({
+          error: "User with same email found in database",
         });
+      user.save(function (err) {
+        if (err) return next(err);
+        res.json({ success: "User successfully created" });
       });
     });
   },
@@ -81,3 +80,28 @@ exports.user_delete = function (req, res, next) {
     }
   );
 };
+
+// user login
+exports.user_login = [
+  body("email").trim().isLength({ min: 1 }).escape(),
+  body("password").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.json(errors.array());
+    User.findOne({ email: req.body.email }).exec(async function (
+      err,
+      found_user
+    ) {
+      if (err) return next(err);
+      if (!found_user) {
+        return res.json({ error: "User not found" });
+      }
+      let result = await bcrypt.compare(req.body.password, found_user.password);
+      if (result) {
+        return res.json(found_user);
+      } else {
+        return res.json("User credentials incorrect");
+      }
+    });
+  },
+];
