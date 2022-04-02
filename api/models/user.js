8ccf53fc8748766a187168298d406fd295bfd0d7
Klay Clarke <clarkeklay@gmail.com>
2022-04-01 22:43:53 -0400

    clean up

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new Schema({
   first_name: { type: String, required: true },
@@ -8,7 +7,5 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
-
-UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
