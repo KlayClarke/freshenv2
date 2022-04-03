67135753aa60270ec7709fcb6ccacd085c1ca860
Klay Clarke <clarkeklay@gmail.com>
2022-04-02 20:31:17 -0400

    when user is logged in, display account and logout links in place of login and join links

--- client/src/components/partials/Nav.jsx
+++ client/src/components/partials/Nav.jsx
@@ -1,41 +1,76 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Nav() {
-  const [isLoggedin, setIsLoggedIn] = useState(false);
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to={"/"} id="header_logo">
-            freshen
-          </Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link to={"/explore"}>Explore</Link>
-        </li>
-        <li>
-          <Link to={"/about"}>About</Link>
-        </li>
-        <li>
-          <span className="divider"></span>
-        </li>
-        <li>
-          <Link to={"/login"}>Log In</Link>
-        </li>
-        <li>
-          <Link
-            to={"/join"}
-            className="button button-s header_join-button border-radius-6px"
-          >
-            Join
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+function Nav({ isLoggedIn }) {
+  if (isLoggedIn == true) {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"} id="header_logo">
+              freshen
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to={"/explore"}>Explore</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <span className="divider"></span>
+          </li>
+          <li>
+            <Link to={"/account"}>Account</Link>
+          </li>
+          <li>
+            <Link
+              to={"/logout"}
+              className="button button-s header_join-button border-radius-6px"
+            >
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  } else {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"} id="header_logo">
+              freshen
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to={"/explore"}>Explore</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <span className="divider"></span>
+          </li>
+          <li>
+            <Link to={"/login"}>Log In</Link>
+          </li>
+          <li>
+            <Link
+              to={"/join"}
+              className="button button-s header_join-button border-radius-6px"
+            >
+              Join
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
