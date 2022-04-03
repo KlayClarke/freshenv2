9aac50f11f3ed89b070fed01559db1df3a48bbed
Klay Clarke <clarkeklay@gmail.com>
2022-04-03 00:03:26 -0400

    handle login (axios call to api) - if credentials are correct, set localstorage and reload page (to change state at root) - else, reload login page so user can try again

--- client/src/components/user/LoginForm.jsx
+++ client/src/components/user/LoginForm.jsx
@@ -1,7 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 function LoginForm() {
+  const [inputValues, setInputValues] = useState({});
+
+  const navigate = useNavigate();
+
+  function handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    setInputValues({ ...inputValues, [name]: value });
+  }
+
+  async function login(e) {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:9000/users/login", {
+      email: inputValues.email,
+      password: inputValues.password,
+    });
+    if (response.data.token && response.data.id) {
+      localStorage.setItem(
+        "freshen_user_data",
+        JSON.stringify({
+          token: response.data.token,
+          id: response.data.id,
+        })
+      );
+      window.location.reload(false);
+    } else {
+      navigate("../login");
+    }
+  }
+
   return (
     <div className="login-content-wrapper">
       <div className="login-content-container">
@@ -12,17 +47,27 @@ function LoginForm() {
           ></FontAwesomeIcon>{" "}
           Log in with Google
         </button>
-        <form action="http://localhost:9000/users/login" method="POST">
+        <form onSubmit={login}>
           <div className="form-section">
             <label htmlFor="email">Email</label>
             <br />
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleInputChange}
+            />
             <br />
           </div>
           <div className="form-section">
             <label htmlFor="password">Password</label>
             <br />
-            <input type="password" id="password" name="password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleInputChange}
+            />
             <br />
           </div>
           <button className="auth-form-submit button border-radius-4px">
