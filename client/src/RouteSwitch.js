acd6513a8e158138eeab6dee9d0e0121e312c5d6
Klay Clarke <clarkeklay@gmail.com>
2022-04-03 00:00:22 -0400

    clean up code, check for localstorage object (tells app whether user is logged in or not) - if user logged in, set user state and pass user to all routes

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import axios from "axios";
 import Home from "./components/Home";
 import Footer from "./components/partials/Footer";
 import Nav from "./components/partials/Nav";
@@ -9,54 +10,36 @@ import JoinForm from "./components/user/JoinForm";
 import LoginForm from "./components/user/LoginForm";
 
 function RouteSwitch() {
-  const [token, setToken] = useState();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState();
 
   useEffect(() => {
-    if (!localStorage.getItem("freshen_user_data")) {
-      setIsLoggedIn(false);
-      setUser({});
+    if (localStorage.getItem("freshen_user_data")) {
+      setUser(JSON.parse(localStorage.getItem("freshen_user_data")));
+    } else {
+      setUser();
     }
-
-    // uncomment code below to simulate loggedin user
-
-    localStorage.setItem(
-      "freshen_user_data",
-      JSON.stringify({
-        token: "",
-        id: "",
-        first_name: "Klay",
-        last_name: "Clarke",
-      })
-    );
-
-    setIsLoggedIn(true);
-    setUser(JSON.parse(localStorage.getItem("freshen_user_data")));
-    console.log("render");
-    console.log(user);
   }, []);
 
   return (
     <BrowserRouter>
-      <Nav isLoggedIn={isLoggedIn} />
+      <Nav user={user} />
       <main>
         <Routes>
+          <Route path="/" element={<Home user={user} />} />
+          <Route path="/explore" element={<SalonList user={user} />} />
           <Route
-            path="/"
-            element={<Home isLoggedIn={isLoggedIn} user={user} />}
+            path="/explore/salons/:salonid"
+            element={<SalonDetail user={user} />}
           />
-          <Route path="/explore" element={<SalonList />} />
-          <Route path="/explore/salons/:salonid" element={<SalonDetail />} />
           <Route
             path="/login"
-            element={isLoggedIn ? <Navigate replace to={"/"} /> : <LoginForm />}
+            element={user ? <Navigate replace to={"/"} /> : <LoginForm />}
           />
           <Route
             path="/join"
-            element={isLoggedIn ? <Navigate replace to={"/"} /> : <JoinForm />}
+            element={user ? <Navigate replace to={"/"} /> : <JoinForm />}
           />
-          <Route path="/about" />
+          <Route path="/about" user={user} />
         </Routes>
       </main>
       <Footer />
