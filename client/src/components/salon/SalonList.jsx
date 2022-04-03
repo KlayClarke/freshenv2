68697743ac68895b73e16ab32379e38187ea04b6
Klay Clarke <clarkeklay@gmail.com>
2022-04-03 19:10:51 -0400

    add loading state, loading spinner for render while loading - add wrappers and containers to salon cards list and individual cards within

--- client/src/components/salon/SalonList.jsx
+++ client/src/components/salon/SalonList.jsx
@@ -1,38 +1,65 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { CirclesWithBar } from "react-loader-spinner";
+import { useEffect, useState } from "react";
 
 function SalonList() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [salons, setSalons] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const response = await axios(`http://localhost:9000/salons/`);
       setSalons(response.data);
-      setIsLoaded(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
     }
     fetchData();
   }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="loading-icon-wrapper">
+        <div className="loading-icon centered">
+          <CirclesWithBar
+            type="Circles"
+            color="rgb(1, 102, 255)"
+            height={160}
+            width={160}
+          />
+        </div>
+      </div>
+    );
   } else {
     return (
-      <div>
-        <h1 className="page-header">Salons</h1>
-        <ul>
-          {salons.map((salon) => (
-            <li key={salon._id}>
-              <div className="salon-card">
-                <Link to={`/salons/${salon._id}`}>{salon.name}</Link>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div className="explore-page-wrapper">
+        <div className="explore-page-container">
+          <ul className="salon-cards-container">
+            {salons.map((salon) => (
+              <Link to={`/salons/${salon._id}`}>
+                <li key={salon._id}>
+                  <div className="salon-card border-radius-4px">
+                    <div className="left-content">
+                      <p className="bold">{salon.name}</p>
+                      <p>{`${salon.street_address} ${salon.city}, ${salon.state} ${salon.zip_code}`}</p>
+                    </div>
+                    <div className="right-content">
+                      {salon.images.length ? (
+                        <img
+                          src={salon.images[0]}
+                          className="border-radius-8px"
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </div>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
