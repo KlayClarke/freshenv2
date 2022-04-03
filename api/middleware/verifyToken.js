08b28c52c101c7dd97cd439cc5ace7ba8471f6d9
Klay Clarke <clarkeklay@gmail.com>
2022-04-03 11:23:27 -0400

    create middleware to verify token on api calls that need authorization (user delete, salon creation, etc -- api calls that require a user to be logged in)

---
+++ api/middleware/verifyToken.js
@@ -0,0 +1,21 @@
+// middleware to verify jwt token on request
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  // check if header is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // split at token
+    const bearer = bearerHeader.split(" ");
+    console.log({ bearer });
+    // get token from array
+    const bearerToken = bearer[1];
+    // set the token
+    req.token = bearerToken;
+    // next
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+module.exports = verifyToken;
