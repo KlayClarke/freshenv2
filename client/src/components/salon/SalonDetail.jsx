023106a27111c500ae68b723f1038d902498863b
Klay Clarke <clarkeklay@gmail.com>
2022-04-04 13:58:06 -0400

    while salon detail page is fetching salon details, display loading spinner

--- client/src/components/salon/SalonDetail.jsx
+++ client/src/components/salon/SalonDetail.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { CirclesWithBar } from "react-loader-spinner";
 
 function SalonDetail() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [salon, setSalon] = useState([]);
 
   const { salonid } = useParams();
@@ -12,25 +13,40 @@ function SalonDetail() {
   useEffect(() => {
     async function fetchData() {
       const response = await axios(`http://localhost:9000/salons/${salonid}`);
-      setSalon(response.data);
-      setIsLoaded(true);
+      console.log(response.data);
+      if (!response.data) {
+        setError(true);
+      } else {
+        setSalon(response.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 250);
+      }
     }
     fetchData();
   }, []);
 
-  console.log(salon);
-
   if (error) {
     return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
+  } else if (loading) {
+    return (
+      <div className="loading-icon-wrapper">
+        <div className="loading-icon centered">
+          <CirclesWithBar
+            type="Circles"
+            color="rgb(1, 102, 255)"
+            height={80}
+            width={80}
+          />
+        </div>
+      </div>
+    );
   } else {
     return (
       <div>
         <h1 className="page-header"></h1>
         <h1>{salon.name}</h1>
         <h3>{salon.street_address}</h3>
-        <h5>Created by {salon.author.email}</h5>
       </div>
     );
   }
