102d7602fc2493825ea9796ee784a44cd39b9ab2
Klay Clarke <clarkeklay@gmail.com>
2022-04-05 22:39:40 -0400

    add salon create form and salon creation handling (axios post, auth token in header)

--- client/src/components/salon/SalonCreateForm.jsx
+++ client/src/components/salon/SalonCreateForm.jsx
@@ -1,17 +1,70 @@
-function SalonCreateForm() {
+import axios from "axios";
+import { useState } from "react";
+
+function SalonCreateForm({ user }) {
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [averagePrice, setAveragePrice] = useState(0);
+  const [images, setImages] = useState([]);
+  const [streetAddress, setStreetAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipCode, setZipCode] = useState("");
+
+  function createSalon(e) {
+    e.preventDefault();
+    axios
+      .post(
+        process.env.REACT_APP_API_SALON_ENDPOINT,
+        {
+          name,
+          type,
+          average_price: averagePrice,
+          images,
+          street_address: streetAddress,
+          city,
+          state,
+          zip_code: zipCode,
+          author: user.id,
+        },
+        {
+          headers: {
+            Authorization: `Basic ${user.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log("salon successfully created", response);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
   return (
     <div className="salon-create-content-wrapper">
       <div className="salon-create-content-container">
-        <form action="">
+        <form onSubmit={createSalon}>
           <div className="form-section">
             <label htmlFor="name">Name</label>
             <br />
-            <input type="text" id="name" />
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="form-section">
             <label htmlFor="type">Type</label>
             <br />
-            <select name="type" id="type" defaultValue={"hybrid"}>
+            <select
+              name="type"
+              id="type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value="" disabled></option>
               <option value="hybrid">Hybrid</option>
               <option value="barbershop">Barbershop</option>
               <option value="salon">Salon</option>
@@ -20,7 +73,13 @@ function SalonCreateForm() {
           <div className="form-section">
             <label htmlFor="average_price">Average Price ($)</label>
             <br />
-            <input type="number" id="average_price" min={0} />
+            <input
+              type="number"
+              id="average_price"
+              min={0}
+              value={averagePrice}
+              onChange={(e) => setAveragePrice(e.target.value)}
+            />
           </div>
           <div className="form-section">
             <label htmlFor="images">Images</label>
@@ -30,22 +89,42 @@ function SalonCreateForm() {
           <div className="form-section">
             <label htmlFor="street_address">Street Address</label>
             <br />
-            <input type="text" id="street_address" />
+            <input
+              type="text"
+              id="street_address"
+              value={streetAddress}
+              onChange={(e) => setStreetAddress(e.target.value)}
+            />
           </div>
           <div className="form-section">
             <label htmlFor="city">City</label>
             <br />
-            <input type="text" id="city" />
+            <input
+              type="text"
+              id="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
           </div>
           <div className="form-section">
             <label htmlFor="state">State</label>
             <br />
-            <input type="text" id="state" />
+            <input
+              type="text"
+              id="state"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+            />
           </div>
           <div className="form-section">
             <label htmlFor="zip_code">Zip Code</label>
             <br />
-            <input type="text" id="zip_code" />
+            <input
+              type="text"
+              id="zip_code"
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
+            />
           </div>
           <button className="button button-s button-success border-radius-4px">
             Create Salon
