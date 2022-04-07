d2a6258c43d864ba038d4e7d0db091ffa55577bf
Klay Clarke <clarkeklay@gmail.com>
2022-04-07 17:05:45 -0400

    add user join and login routes to index router file

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const user_controller = require("../controllers/userController");
 
 router.get("/", (req, res) => {
   res.render("home");
 });
+
+router
+  .route("/join")
+  .get(user_controller.user_join_form)
+  .post(user_controller.user_create);
+
+router
+  .route("/login")
+  .get(user_controller.user_login_form)
+  .post(user_controller.user_login);
 
 module.exports = router;
