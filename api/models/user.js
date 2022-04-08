262967982cc6386a28670340bcf42031a038aed4
Klay Clarke <clarkeklay@gmail.com>
2022-04-08 12:18:18 -0400

    change params, add passport plugin to schema

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
 });
+
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
