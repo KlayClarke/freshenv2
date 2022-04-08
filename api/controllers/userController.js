d8a63ffc6f8fa0664c1bbd5359ba7730f269ba8a
Klay Clarke <clarkeklay@gmail.com>
2022-04-08 12:09:03 -0400

    handle user join, login and logout post requests

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -5,117 +5,121 @@ const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const { body, validationResult, sanitizeBody } = require("express-validator");
 
-exports.user_join_form = (req, res) => {
+// user join get
+exports.user_join_get = (req, res) => {
   res.render("user_join_form");
 };
 
-exports.user_login_form = (req, res) => {
+// user login get
+exports.user_login_get = (req, res) => {
   res.render("user_login_form");
 };
 
-// return all users
-exports.user_list = function (req, res) {
-  User.find().exec(function (err, list_users) {
-    if (err) return next(err);
-    return res.json(list_users);
-  });
-};
-
-// return specific user
-exports.user_detail = function (req, res, next) {
-  async.parallel(
-    {
-      user: function (callback) {
-        User.findById(req.params.userid).exec(callback);
-      },
-    },
-    function (err, results) {
-      if (err) return next(err);
-      return res.json(results.user);
-    }
-  );
-};
-
-// user create
-exports.user_create = [
-  body("first_name").trim().isLength({ min: 1 }).escape(),
-  body("last_name").trim().isLength({ min: 1 }).escape(),
+// user join post
+exports.user_join_post = [
   body("email").trim().isLength({ min: 1 }).escape(),
+  body("username").trim().isLength({ min: 1 }).escape(),
   body("password").trim().isLength({ min: 1 }).escape(),
   async (req, res, next) => {
     const errors = validationResult(req);
-    let hashedPassword = await bcrypt.hash(req.body.password, 10);
-    let user = new User({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-    if (!errors.isEmpty()) return res.json(errors.array());
-    User.findOne({ email: req.body.email }).exec(function (err, found_user) {
+    if (!errors.isEmpty()) {
+      req.flash("error", errors.array());
+      return res.redirect("/join");
+    }
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
       if (err) return next(err);
-      if (found_user) return res.render("");
-      user.save(function (err) {
-        if (err) return next(err);
-        res.render("home");
-      });
+      req.flash("success", "Welcome to freshen");
+      const redirectUrl = req.session.returnTo || "/";
+      delete req.session.returnTo;
+      res.redirect(redirectUrl);
     });
   },
 ];
 
-// user update
-exports.user_update = function (req, res) {};
-
-// user delete
-exports.user_delete = function (req, res, next) {
-  async.parallel(
-    {
-      user: function (callback) {
-        User.findById(req.params.userid).exec(callback);
-      },
-    },
-    function (err, results) {
-      if (err) return next(err);
-      jwt.verify(req.token, "secretkey", (err, authData) => {
-        if (err) return res.sendStatus(403);
-        User.findByIdAndDelete(req.params.userid, function deleteUser(err) {
-          if (err) return next(err);
-          return res.send("User successfully deleted");
-        });
-      });
+// user login post
+exports.user_login_post = [
+  body("username").trim().isLength({ min: 1 }).escape(),
+  body("password").trim().isLength({ min: 1 }).escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log("errors", errors);
+      req.flash("error", errors.array());
+      res.redirect("/login");
     }
-  );
+    req.flash("success", "Welcome back!");
+    const redirectUrl = req.session.returnTo || "/";
+    delete req.session.returnTo;
+    console.log("successfully logged in");
+    res.redirect(redirectUrl);
+  },
+];
+
+exports.user_logout_post = (req, res, next) => {
+  req.logout();
+  req.flash("success", "Successfully logged out");
+  res.redirect("/");
 };
 
-// user login
-exports.user_login = [
-  body("email").trim().isLength({ min: 1 }).escape(),
-  body("password").trim().isLength({ min: 1 }).escape(),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.json(errors.array());
-    User.findOne({ email: req.body.email }).exec(async function (
-      err,
-      found_user
-    ) {
-      if (err) return next(err);
-      if (!found_user) {
-        return res.json({ error: "User not found" });
-      }
-      // check if request body password matches db unhashed password
-      let result = await bcrypt.compare(req.body.password, found_user.password);
-      if (result) {
-        jwt.sign(
-          { email: found_user.email, password: found_user.password },
-          "secretkey",
-          (err, token) => {
-            if (err) return next(err);
-            res.json({ token, id: found_user._id });
-          }
-        );
-      } else {
-        return res.render("home");
-      }
-    });
-  },
-];
+// // user join post
+// exports.user_join_post = [
+//   body("first_name").trim().isLength({ min: 1 }).escape(),
+//   body("last_name").trim().isLength({ min: 1 }).escape(),
+//   body("email").trim().isLength({ min: 1 }).escape(),
+//   body("password").trim().isLength({ min: 1 }).escape(),
+//   async (req, res, next) => {
+//     const errors = validationResult(req);
+//     let hashedPassword = await bcrypt.hash(req.body.password, 10);
+//     let user = new User({
+//       first_name: req.body.first_name,
+//       last_name: req.body.last_name,
+//       email: req.body.email,
+//       password: hashedPassword,
+//     });
+//     if (!errors.isEmpty()) return res.json(errors.array());
+//     User.findOne({ email: req.body.email }).exec(function (err, found_user) {
+//       if (err) return next(err);
+//       if (found_user) return res.render("");
+//       user.save(function (err) {
+//         if (err) return next(err);
+//         res.render("home");
+//       });
+//     });
+//   },
+// ];
+
+// // user login post
+// exports.user_login_post = [
+//   body("email").trim().isLength({ min: 1 }).escape(),
+//   body("password").trim().isLength({ min: 1 }).escape(),
+//   (req, res, next) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) return res.json(errors.array());
+//     User.findOne({ email: req.body.email }).exec(async function (
+//       err,
+//       found_user
+//     ) {
+//       if (err) return next(err);
+//       if (!found_user) {
+//         return res.json({ error: "User not found" });
+//       }
+//       // check if request body password matches db unhashed password
+//       let result = await bcrypt.compare(req.body.password, found_user.password);
+//       if (result) {
+//         jwt.sign(
+//           { email: found_user.email, password: found_user.password },
+//           "secretkey",
+//           (err, token) => {
+//             if (err) return next(err);
+//             res.json({ token, id: found_user._id });
+//           }
+//         );
+//       } else {
+//         return res.render("home");
+//       }
+//     });
+//   },
+// ];
