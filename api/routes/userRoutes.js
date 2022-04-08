eed9f0a95aa2be9590407b3bec5d041cc6296074
Klay Clarke <clarkeklay@gmail.com>
2022-04-08 14:27:31 -0400

    put all user requests in user router

---
+++ api/routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const user_controller = require("../controllers/userController");
+
+router
+  .route("/join")
+  .get(user_controller.user_join_get)
+  .post(user_controller.user_join_post);
+
+router
+  .route("/login")
+  .get(user_controller.user_login_get)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    user_controller.user_login_post
+  );
+
+router.get("/logout", user_controller.user_logout_post);
+
+module.exports = router;
