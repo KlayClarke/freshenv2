fae2cf76f4f98222746a4785c355a1652479b9d7
Klay Clarke <clarkeklay@gmail.com>
2022-04-09 12:25:52 -0400

    make alert fade away, remove console logs

--- api/public/javascripts/hideAlerts.js
+++ api/public/javascripts/hideAlerts.js
@@ -1,10 +1,7 @@
-console.log("js testing");
-
 if (document.querySelector(".alert")) {
   const alert = document.querySelector(".alert");
-  console.log("start");
   setTimeout(() => {
-    console.log("fin");
-    alert.style.display = "none";
-  }, 5000);
+    alert.style.opacity = 0;
+    alert.style.transition = "display 0s 2s, opacity 3s linear";
+  }, 3500);
 }
