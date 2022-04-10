5a3aab9f300ddee40637b3c566f142b5cbe6e516
Klay Clarke <clarkeklay@gmail.com>
2022-04-10 09:31:32 -0400

    handle user account page get and user account delete post requests

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -95,62 +95,32 @@ exports.user_logout_post = (req, res, next) => {
   res.redirect("/");
 };
 
-// // user join post
-// exports.user_join_post = [
-//   body("first_name").trim().isLength({ min: 1 }).escape(),
-//   body("last_name").trim().isLength({ min: 1 }).escape(),
-//   body("email").trim().isLength({ min: 1 }).escape(),
-//   body("password").trim().isLength({ min: 1 }).escape(),
-//   async (req, res, next) => {
-//     const errors = validationResult(req);
-//     let hashedPassword = await bcrypt.hash(req.body.password, 10);
-//     let user = new User({
-//       first_name: req.body.first_name,
-//       last_name: req.body.last_name,
-//       email: req.body.email,
-//       password: hashedPassword,
-//     });
-//     if (!errors.isEmpty()) return res.json(errors.array());
-//     User.findOne({ email: req.body.email }).exec(function (err, found_user) {
-//       if (err) return next(err);
-//       if (found_user) return res.render("");
-//       user.save(function (err) {
-//         if (err) return next(err);
-//         res.render("home");
-//       });
-//     });
-//   },
-// ];
+// user account get
+exports.user_account_get = (req, res, next) => {
+  res.render("user_account");
+};
 
-// // user login post
-// exports.user_login_post = [
-//   body("email").trim().isLength({ min: 1 }).escape(),
-//   body("password").trim().isLength({ min: 1 }).escape(),
-//   (req, res, next) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) return res.json(errors.array());
-//     User.findOne({ email: req.body.email }).exec(async function (
-//       err,
-//       found_user
-//     ) {
-//       if (err) return next(err);
-//       if (!found_user) {
-//         return res.json({ error: "User not found" });
-//       }
-//       // check if request body password matches db unhashed password
-//       let result = await bcrypt.compare(req.body.password, found_user.password);
-//       if (result) {
-//         jwt.sign(
-//           { email: found_user.email, password: found_user.password },
-//           "secretkey",
-//           (err, token) => {
-//             if (err) return next(err);
-//             res.json({ token, id: found_user._id });
-//           }
-//         );
-//       } else {
-//         return res.render("home");
-//       }
-//     });
-//   },
-// ];
+// user account delete post
+exports.user_account_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      user: function (callback) {
+        User.findById(req.user._id).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        console.log("akjsdjksdl");
+        req.flash("error", "Something went wrong! Please try again.");
+        return res.redirect("/account");
+      }
+      if (results.user) {
+        User.findByIdAndDelete(req.user._id, function deleteUser(err) {
+          if (err) return next(err);
+          req.flash("success", "Your account has been deleted");
+          res.redirect("/");
+        });
+      }
+    }
+  );
+};
