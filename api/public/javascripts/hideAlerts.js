e6ebe0edb899edc326bf20558594105037bd1752
Klay Clarke <clarkeklay@gmail.com>
2022-04-09 21:00:36 -0400

    hide alert quicker

--- api/public/javascripts/hideAlerts.js
+++ api/public/javascripts/hideAlerts.js
@@ -2,6 +2,6 @@ if (document.querySelector(".alert")) {
   const alert = document.querySelector(".alert");
   setTimeout(() => {
     alert.style.opacity = 0;
-    alert.style.transition = "display 0s 2s, opacity 3s linear";
-  }, 3500);
+    alert.style.transition = "display 0s 1s, opacity 1.5s linear";
+  }, 2000);
 }
