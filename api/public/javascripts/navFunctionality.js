b5b0342288533a721dc135447f3f102ff1bc6ab1
Klay Clarke <clarkeklay@gmail.com>
2022-04-12 14:36:26 -0400

    cover main content with opaque div when nav_overlay is open

--- api/public/javascripts/navFunctionality.js
+++ api/public/javascripts/navFunctionality.js
@@ -2,11 +2,15 @@ function toggleNav() {
   if (document.querySelector("#nav_overlay").classList.contains("show")) {
     document.querySelector("#nav_overlay").classList.remove("show");
     document.querySelector("#nav_overlay").classList.add("hide");
+    document.querySelector("#overlay").classList.remove("show");
+    document.querySelector("#overlay").classList.add("hide");
   } else if (
     document.querySelector("#nav_overlay").classList.contains("hide")
   ) {
     document.querySelector("#nav_overlay").classList.remove("hide");
     document.querySelector("#nav_overlay").classList.add("show");
+    document.querySelector("#overlay").classList.remove("hide");
+    document.querySelector("#overlay").classList.add("show");
   }
 }
 
