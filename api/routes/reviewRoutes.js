4a0f2f00f82f774655ef6d0fc0ecaf48d39b1d19
Klay Clarke <clarkeklay@gmail.com>
2022-04-12 20:49:57 -0400

    remove defunct import pt.2

--- api/routes/reviewRoutes.js
+++ api/routes/reviewRoutes.js
@@ -5,12 +5,12 @@ const { verifyToken } = require("../middleware/verifyToken");
 
 router
   .route("/")
-  .get(logUrl, review_controller.review_list)
+  .get(review_controller.review_list)
   .post(verifyToken, review_controller.review_create);
 
 router
   .route("/:reviewid")
-  .get(logUrl, review_controller.review_detail)
+  .get(review_controller.review_detail)
   .delete(verifyToken, review_controller.review_delete);
 
 module.exports = router;
--- api/routes/salonRoutes.js
+++ api/routes/salonRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const salon_controller = require("../controllers/salonController");
 const { verifyToken } = require("../middleware/verifyToken");
 
-router.get("/", logUrl, salon_controller.salon_explore_get);
+router.get("/", salon_controller.salon_explore_get);
 
-router.get("/create", logUrl, salon_controller.salon_create_get);
+router.get("/create", salon_controller.salon_create_get);
 
 router
   .route("/:salonid")
-  .get(logUrl, salon_controller.salon_detail_get)
+  .get(salon_controller.salon_detail_get)
   .post(salon_controller.salon_update_post);
 
 module.exports = router;
