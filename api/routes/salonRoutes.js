2b73a3128231b58f2c9daccf0d1b6824e9d2269f
Klay Clarke <clarkeklay@gmail.com>
2022-04-16 18:00:06 -0400

    redirect to sorted explore when just '/explore' is requested

--- api/routes/salonRoutes.js
+++ api/routes/salonRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const salon_controller = require("../controllers/salonController");
 const { verifyToken } = require("../middleware/verifyToken");
 
+router.get("/", function (req, res, next) {
+  res.redirect("explore/sort/name");
+});
+
 router.route("/sort/:sortby").get(salon_controller.salon_explore_get);
 
 router
