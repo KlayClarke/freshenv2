12251dd2de818a671af6272492b0b8313e3f3dc4
Klay Clarke <clarkeklay@gmail.com>
2022-04-19 16:36:56 -0400

    remove alert element from document when alert fades away

--- public/javascripts/hideAlerts.js
+++ public/javascripts/hideAlerts.js
@@ -3,5 +3,8 @@ if (document.querySelector(".alert")) {
   setTimeout(() => {
     alert.style.opacity = 0;
     alert.style.transition = "display 0s 1s, opacity 1.5s linear";
+    setTimeout(() => {
+      alert.style.display = "none";
+    }, 1500);
   }, 2000);
 }
