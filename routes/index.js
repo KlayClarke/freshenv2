c94063edd2b770b3d681f7c629915043327ccc67
Klay Clarke <clarkeklay@gmail.com>
2022-04-21 20:21:41 -0400

    add salons api route to index router (returns salon list as json)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const async = require("async");
+const Salon = require("../models/salon");
+const salon_controller = require("../controllers/salonController");
 
 router.get("/", (req, res) => {
   res.render("home");
 });
+
+// salons api call
+router.route("/api/salons").get(salon_controller.get_salon_list);
 
 module.exports = router;
