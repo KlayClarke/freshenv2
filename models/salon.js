9da298a85973f7b1b61b87a98d4cc6c3f6de9c24
Klay Clarke <clarkeklay@gmail.com>
2022-04-21 20:14:54 -0400

    add virtual to salonSchema that returns html for an A tag... to be displayed on our cluster map, when clicked, this A tag will send users to the salon's detail page

--- models/salon.js
+++ models/salon.js
@@ -2,28 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review");
 
-const SalonSchema = new Schema({
-  name: { type: String, required: true },
-  type: { type: String, required: true },
-  average_price: { type: Number, required: true },
-  image: { type: String, required: false },
-  street_address: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  zip_code: { type: String, required: true },
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const SalonSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    average_price: { type: Number, required: true },
+    image: { type: String, required: false },
+    street_address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip_code: { type: String, required: true },
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
   },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+  opts
+);
+
+SalonSchema.virtual("properties.mapboxClusterHTML").get(function () {
+  return `<h3><a href='/explore/detail/${this._id}' class='blue underline'>${this.name}</a></h3>`;
 });
 
 SalonSchema.post("findOneAndDelete", async function (salon) {
